05561c64566f6964e0d29e2527f2987165b24d87
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-05 11:40:53 -0300

    :sparkles: feat: create entity candidate in db

--- src/main/java/br/com/carlosalexandredevv/management_job/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/modules/candidate/CandidateEntity.java
@@ -1,22 +1,34 @@
 package br.com.carlosalexandredevv.management_job.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank
-  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
-  private String username;
+    @Pattern(regexp = "\\S+", message = "O campo username não deve conter espaço")
+    private String username;
+
     @Email(message="O campo email deve ser um email válido")
     private String email;
 
@@ -25,4 +37,7 @@ public class CandidateEntity {
     
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
