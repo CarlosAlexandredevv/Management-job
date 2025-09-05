0679116fe0116f3db7c12f266be9513623ae15ca
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-05 19:41:41 -0300

    :sparkles: feat: init authenticated user

--- src/main/java/br/com/carlosalexandredevv/management_job/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/modules/company/repositories/CompanyRepository.java
@@ -9,4 +9,6 @@ import br.com.carlosalexandredevv.management_job.modules.company.entities.Compan
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
     Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+    
+    Optional<CompanyEntity> findByUsername(String username);
 }
--- src/main/java/br/com/carlosalexandredevv/management_job/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/modules/company/useCases/CreateCompanyUseCase.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.com.carlosalexandredevv.management_job.modules.company.repositories.CompanyRepository;
 import br.com.carlosalexandredevv.management_job.exceptions.UserFoundException;
 import br.com.carlosalexandredevv.management_job.modules.company.entities.CompanyEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class CreateCompanyUseCase {
@@ -13,11 +14,17 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity){
 
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail()).ifPresent(company -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
