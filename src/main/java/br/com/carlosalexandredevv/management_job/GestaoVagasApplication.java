a88657b301ddcaf2868af026a06c4d242484dc17
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-09 23:51:53 -0300

    :pencil: docs: create docs swagger

--- src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.carlosalexandredevv.management_job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Management Job", version = "1.0", description = "Api for manage jobs"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
