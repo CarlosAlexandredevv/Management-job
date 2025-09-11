894e755c0114dbec5aab4cff6d661199a8692084
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-10 21:50:02 -0300

    :books: docs: auth

--- src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Management Job", version = "1.0", description = "Api for manage jobs"))
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
