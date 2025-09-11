87a27343293893c07e8cd423bbaa6a8bf41c587a
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-10 23:17:09 -0300

    :books: refactor: config swagger

--- src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
+++ src/main/java/br/com/carlosalexandredevv/management_job/GestaoVagasApplication.java
@@ -3,17 +3,9 @@ package br.com.carlosalexandredevv.management_job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Management Job", version = "1.0", description = "Api for manage jobs"))
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
